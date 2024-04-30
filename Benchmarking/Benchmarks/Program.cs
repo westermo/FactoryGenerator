e9c92fb5ff26aa52c150695ca79573a053716732
caran <carl.andersson@westermo.com>
2024-04-30 10:06:37 +0200

    Included basic benchmarking

---
+++ Benchmarking/Benchmarks/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FactoryGenerator;
+using Inherited;
+using Inheritor;
+using Inheritor.Generated;
+
+namespace Benchmarks;
+
+public class ResolveBenchmarks
+{
+    private readonly IContainer m_container = new DependencyInjectionContainer(default, default, default!);
+
+    [Benchmark]
+    public ChainA ResolveChain() => m_container.Resolve<ChainA>();
+
+    [Benchmark]
+    public IScoped ResolveScoped() => m_container.Resolve<IScoped>();
+
+    [Benchmark]
+    public ISingleton ResolveSingleton() => m_container.Resolve<ISingleton>();
+
+    [Benchmark]
+    public IOverridable ResolveTransient() => m_container.Resolve<IOverridable>();
+
+    [Benchmark]
+    public IContainer Create() => new DependencyInjectionContainer(default, default, default!);
+}
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ResolveBenchmarks>();
+    }
+}
