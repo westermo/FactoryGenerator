de1e512d4887a2c16f120259c794b65bf3440d35
Iacentis <CarlJFandersson@gmail.com>
2026-03-05 19:08:57 +0100

    Add ASP.NET Core integration with FactoryGenerator middleware and related tests

---
+++ FactoryGenerator.Extensions.AspNetCore/ServiceProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FactoryGenerator;
+
+namespace FactoryGenerator.Extensions.AspNetCore;
+
+public static class ServiceProviderExtensions
+{
+    public static IContainer ToContainer(this IServiceProvider serviceProvider)
+    {
+        return new ServiceProviderAdapter(serviceProvider);
+    }
+}
