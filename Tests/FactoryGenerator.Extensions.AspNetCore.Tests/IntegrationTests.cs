534be099235d8486942a3d7c2d2908e2af9ad658
caran <carl.andersson@westermo.com>
2026-04-10 17:38:20 +0200

    TUnit Migration

--- Tests/FactoryGenerator.Extensions.AspNetCore.Tests/IntegrationTests.cs
+++ Tests/FactoryGenerator.Extensions.AspNetCore.Tests/IntegrationTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Shouldly;
-using Xunit;
 using FactoryGenerator;
 using FactoryGenerator.Attributes;
 using FactoryGenerator.Extensions.AspNetCore;
@@ -37,7 +36,7 @@ public class OtherService : IOtherService
 
 public class IntegrationTests
 {
-    [Fact]
+    [Test]
     public async Task Middleware_Integrates_FactoryGenerator_With_RequestServices()
     {
         // Setup
